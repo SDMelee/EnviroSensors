04deb23a61b626fd3261fb97aec8ffc5988ab1f1
SDMelee <SDMelee@users.noreply.github.com>
2017-08-15 22:47:15 -0400

    Update MQ4.cpp
    Added file description.

--- Sensors/MQ4.cpp
+++ Sensors/MQ4.cpp
@@ -1,9 +1,50 @@
+/****************************************************************************************************
+MQ4.cpp
+MQ4 Methane and Natural Gas Sensor Arduino library
+Amelia Peterson w/ CSDT @ RPI
+Contact: petera7@rpi.edu
+Last Updated: 08/15/2017
+
+Visit our site at: www.csdt.rpi.edu
+
+Description:
+_calculateRs() calculates the sensor resistance from the voltage divider equation:
+
+       Rs          Rl
+VCC----/\/\/\/\----/\/\/\/\---GND
+                ^
+                |____Vrl
+
+						Vrl=Vcc*(Rl/(Rs+Rl))
+						Rs=5*(Rl/Vrl)-Rl
+
+  The gas concentration equation used in readValue(String) was determined from a best fit to the
+Rs/R0 plot in the datasheet. The temperature and humidity adjustments were calculated similarly.
+
+!!PLEASE READ THE DATASHEET BEFORE USE!!
+
+Some important notes:
+	- Do not expose the sensor to significant moisture
+	- Do not connect the heater to the Arduino directly - it must be connected to an external
+	  power source to provide enough current.
+	- Do NOT apply voltage over 5V to the heater.
+	- Check that your load resistor matches the _Rl member variable.
+	- If soldering, check data sheet for proper heat settings.
+	- Calibrate in clean air with no methane or natural gases.
+	- MQ4 sensor needs 48 HOURS of preheat time to operate correctly
+
+To do:
+	- Add ability to use analog or digital potentiometer (AD5171) to change sensitivity
+	- Check accuracy of equations against known concentrations of methane and natural gas
+	- Determine Rs/Rs0=f(T, %RH) under varying gas conditions (100ppm, 500ppm, 1000ppm, 10000ppm)
+****************************************************************************************************/
+
 #include "MQ4.h"
 
 float MQ4::_calculateRs(){
 	float analogValue=float(analogRead(_analogPin));
-	float V_Rl = analogValue*(5.0/1023);	// Convert to Voltage
-	return (((5.0*_Rl)/V_Rl)-_Rl)/1000;
+	float V_Rl = analogValue*(5.0/1023);						// Convert to Voltage
+	return (((5.0*_Rl)/V_Rl)-_Rl)/1000;							// Use voltage divider equation and convert to kOhm
 }
 
 float MQ4::readValue(String units){
@@ -25,7 +66,7 @@ float MQ4::readValue(String units){
 
 
 void MQ4::_calculateR0(){
-	//Serial.println("Calibrating sensor for R0, make sure sensor is in clean air.");
+	Serial.println("Calibrating sensor for R0, make sure sensor is in clean air.");
 	_R0=_calculateRs();
 }
 
